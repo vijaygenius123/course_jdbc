8deba1e72c0765e5b7264cb5332bac92182e00ad
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-16 22:12:50 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/learning/course_jdbc/CourseJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.course_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/learning/course_jdbc/CourseJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.course_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
